301779e4ecec299d3e84820f3935555397c3b623
Traap <gary.a.howard@mac.com>
2017-03-18 10:01:12 -0700

    Issue #4 - print bootstrap.yaml contents.

--- yamlreader.hs
+++ yamlreader.hs
@@ -4,98 +4,38 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
----------------------------------------------------------------------------- }}}
--- {{{ Required imports
-import           Data.Yaml
+
+import Traap.DataTypes
+import Data.Yaml
+import qualified Data.ByteString as BL 
 import qualified Data.Text as T
-import qualified Data.ByteString as BL
-import           GHC.Generics
----------------------------------------------------------------------------- }}}
--- {{{ Symbolic Links
-data Symlinks = SYMLINKS
-  { symlinks :: [Symlink]
-  } deriving (Show, Generic, FromJSON)
 
-data Symlink = SYMLINK
-  { file :: T.Text
-  , link :: T.Text
-  , directory :: Bool
-  } deriving (Show, Generic, FromJSON)
----------------------------------------------------------------------------- }}}
--- {{{ Repositories
-data Repos = REPOS
-  { repos :: [Repo]
-  } deriving (Show, Generic, FromJSON)
 
-data Repo = REPO
-  { url :: T.Text
-  , paths ::  [Path]
-  } deriving (Show, Generic, FromJSON)
-
-data Path = PATH
-  { source :: T.Text
-  , target :: T.Text
-  } deriving (Show, Generic, FromJSON)
----------------------------------------------------------------------------- }}}
--- {{{ Installations
-data Installations = INSTALLATIONS
-  { installations :: [Os]
-  } deriving (Show, Generic, FromJSON)
-
-data Os = OS
-  { name :: T.Text
-  , command :: [Command]
-  } deriving (Show, Generic, FromJSON)
-
-data Command = COMMAND
-  { sudo :: Bool
-  , program :: T.Text
-  , argument :: T.Text
-  } deriving (Show, Generic, FromJSON)
----------------------------------------------------------------------------- }}}
--- {{{ toSymlinks
-toSymlinks :: Symlinks -> [T.Text]
-toSymlinks = map toSymlink . symlinks
----------------------------------------------------------------------------- }}}
--- {{{ toSymlink
-toSymlink :: Symlink -> T.Text
-toSymlink s = mconcat ["ln -s", file s, " ", link s] 
----------------------------------------------------------------------------- }}}
--- {{{ toRepos
-toRepos :: Repos -> [T.Text]
-toRepos = concatMap toRepo . repos 
----------------------------------------------------------------------------- }}}
--- {{{ toRepo
-toRepo :: Repo -> [T.Text]
-toRepo REPO {..} = map (toPath url) paths
----------------------------------------------------------------------------- }}}
--- {{{ toPath
-toPath :: T.Text -> Path -> T.Text
-toPath u p = mconcat ["git clone ", u, "/", source p, target p] 
----------------------------------------------------------------------------- }}}
--- {{{ toInstallations
-toInstallations :: Installations -> [T.Text]
-toInstallations = concatMap toOs . installations 
----------------------------------------------------------------------------- }}}
--- {{{ toOs
-toOs :: Os -> [T.Text]
-toOs = map toCommand . command 
----------------------------------------------------------------------------- }}}
--- {{{ toCommand
-toCommand :: Command -> T.Text
-toCommand c = mconcat [program c, argument c] 
----------------------------------------------------------------------------- }}}
 -- {{{ Main
 main :: IO ()
 main = do
   contents <- BL.readFile "bootstrap.yaml"
 
   putStrLn "\nSymoblic Links"
-  either putStrLn (print . toSymlinks) $ decodeEither contents
+  mapM_ printSymLink $ decodeEither contents
 
   putStrLn "\nRepositories"
-  either putStrLn (print . toRepos) $ decodeEither contents
+  mapM_ printRepo $ decodeEither contents
 
   putStrLn "\nInstallations"
-  either putStrLn (print . toInstallations) $ decodeEither contents
+  mapM_ printInstallations $ decodeEither contents
+
+printSymLink :: Symlinks -> IO () 
+printSymLink s = mapM_ (print' . T.unpack) $ toSymlinks s CREATE
+
+printRepo :: Repos -> IO ()
+printRepo r = mapM_ (print' . T.unpack) $ toRepos r CLONE
+
+printInstallations :: Installations -> IO ()
+printInstallations i = mapM_ (print' . T.unpack) $ toInstallations i INSTALL 
+
+print' :: String -> IO ()
+print' s = do
+  putStrLn s
+  return ()
 ---------------------------------------------------------------------------- }}}
