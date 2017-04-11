48d1ed48484c2fefdf2eddab0ede12cbb1e001d4
Traap <gary.a.howard@mac.com>
2017-04-11 13:52:50 -0700

    Issue #7 - Initial commit.

--- install.sh
+++ install.sh
@@ -15,30 +15,66 @@ fctTorun="";  # The function the uses requested to run.
 # ------------------------------------------------------------------------------
 # Orchestrate the show.
 # ------------------------------------------------------------------------------
-main() {
+main {
   parseOptions $@
   if [ $helpFlag == 1 -o $errorFlag == 1 ]; then
     showHelp
   elif [ $allFlag == 1 ]; then
     if [[ ${OSTYPE} =~ "linux" ]]; then
-      runFunction aptUpdate
-      runFunction installHaskell 
+      source linux/install.sh
     elif [[ ${OSTYPE} =~ "darwin" ]]; then
-      runFunction installHomebrew
-      runFunction installHaskell 
+      source darwin/install.sh
     else
       exitProgram
     fi
-    runFunction runBootstrap
+    runFunction orchestrate
   else
     runFunction $fctToRun
   fi
+}
+# ------------------------------------------------------------------------------
+# 
+# ------------------------------------------------------------------------------
+orchestrate {
+    funFunction beforeInstall
+    runFunction install
+    runFunction runBootstrap
+    runFunction afterInstall
+}
+
+# ------------------------------------------------------------------------------
+# 
+# ------------------------------------------------------------------------------
+beforeInstall {
+  runFunction _beforeInstall
+}
+
+# ------------------------------------------------------------------------------
+# 
+# ------------------------------------------------------------------------------
+install {
+  runFunction _install
+}
+
+# ------------------------------------------------------------------------------
+# A function to bootstrap your personalization.  
+# ------------------------------------------------------------------------------
+runBootstrap {
+  cd ${HOME}/bootstrap
+  _runBootstrap
+}
+
+# ------------------------------------------------------------------------------
+# 
+# ------------------------------------------------------------------------------
+afterInstall {
+  runFunction _afterInstall
 }
 
 # ------------------------------------------------------------------------------
 # Show the help text for the script and then terminate execution.
 # ------------------------------------------------------------------------------
-function showHelp {
+showHelp {
    echo
    echo "Usage: install [OPTIONS]"
    echo "Run install to bootstrap a configurable bootstraper."
@@ -49,8 +85,9 @@ function showHelp {
    echo "  --debug         Echo enviroment variables to sysout."
    echo
    echo "  --function=     Internal function name to run."
-   echo "             installHaskell"
-   echo "             installHomeBrew"
+   echo "             beforeInstall"
+   echo "             install"
+   echo "             afterInstall"
    echo
    echo "  --help          Display this help message."
 }
@@ -64,7 +101,7 @@ function showHelp {
 # Arguments:
 # A non-empty array containing the command line options.
 # ------------------------------------------------------------------------------
-function parseOptions {
+parseOptions {
     # Iterate through the list of options.
     for var in "$@"
     do
@@ -97,7 +134,7 @@ function parseOptions {
 # #Arguments:
 # $1 is the function to run
 # ------------------------------------------------------------------------------
-runFunction() {
+runFunction {
   cd ${HMST_ROOT}
   echo "*** Entering runFunction with" $1
   time $1
@@ -108,86 +145,12 @@ runFunction() {
 # ------------------------------------------------------------------------------
 # A function to exit this program and print a message. 
 # ------------------------------------------------------------------------------
-exitProgram() {
+exitProgram {
   echo "${OSTYPE} is not installed.  Program exiting."
   exit;
 }
 
-# ------------------------------------------------------------------------------
-# A function to to apt-get update Linux 
-# ------------------------------------------------------------------------------
-aptUpdate() {
-  if [[ ${OSTYPE} =~ "linux" ]]; then
-    curl -sSL https://get.haskellstack.org/ | sh
-  else
-    exitProgram
-  fi
-}
 
-# ------------------------------------------------------------------------------
-# A function to install Haskell dependencies. 
-# ------------------------------------------------------------------------------
-haskellDependencies() {
-  if [[ ${OSTYPE} =~ "linux" ]]; then
-    sudo apt-get -y install \
-      g++ \
-      gcc \
-      libc6-dev \
-      libffi-dev \
-      libgmp-dev \
-      make \
-      xz-utils \
-      zlib1g-dev \
-      gnupg \
-      python
-  else
-    exitProgram
-  fi
-}
-
-# ------------------------------------------------------------------------------
-# A function to install Homebrew.
-# ------------------------------------------------------------------------------
-installHomebrew() {
-  if [[ ${OSTYPE} =~ "darwin" ]]; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  else
-    exitProgram
-  fi
-}
-
-# ------------------------------------------------------------------------------
-# A function to install haskell.
-# ------------------------------------------------------------------------------
-installHaskell() {
-  if [[ ${OSTYPE} =~ "linux" ]]; then
-    haskellDependencies
-    curl -sSL https://get.haskellstack.org/ | sh
-  elif [[ ${OSTYPE} =~ "darwin" ]]; then
-    brew cask install haskell-platform
-  else
-    exitProgram 
-  fi
-}
-
-# ------------------------------------------------------------------------------
-# A function to bootstrap your personalization.  
-# ------------------------------------------------------------------------------
-runBootstrap() {
-  cd ${HOME}/bootstrap
-  if [[ ${OSTYPE} =~ "linux" ]]; then
-    stack init --force
-    stack setup
-    stack build
-    stack exec -- bootstrap
-  elif [[ ${OSTYPE} =~ "darwin" ]]; then
-    cabal install
-    ~/Library/Haskell/bin/bootstrap -f bootstrap.yaml
-  else
-    exitProgram 
-  fi
-
-}
 
 # ------------------------------------------------------------------------------
 # Kick start this script.
